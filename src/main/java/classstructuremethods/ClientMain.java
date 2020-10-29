c1f25f1d6fff4d0abfebb825d28eaa33a7912c8f
Engomo <laszlo.vadkert@gmail.com>
2020-10-29 19:21:33 +0100

    Music és Song kész

---
+++ src/main/java/classstructuremethods/ClientMain.java
@@ -0,0 +1,14 @@
+package classstructuremethods;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.setName("Sanyi");
+        client.setYear(1987);
+        client.setAdress("6000 Kecskemét Petőfi Sándor u. 48");
+        System.out.println(client.getName() + "\n" + client.getYear() + "\n" + client.getAdress());
+
+        client.migrate("6000 Kecskemét Új u. 111");
+        System.out.println("Az új címed: " + client.getAdress());
+    }
+}
