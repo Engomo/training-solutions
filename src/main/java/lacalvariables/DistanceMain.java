5876020ac0dd4b33627dd46ffdcd3bd194e2741d
Engomo <laszlo.vadkert@gmail.com>
2020-11-02 15:46:45 +0100

    literálok és típusok feladat

---
+++ src/main/java/lacalvariables/DistanceMain.java
@@ -0,0 +1,12 @@
+package lacalvariables;
+
+public class DistanceMain {
+    public static void main(String[] args) {
+        Distance distance = new Distance(66.6,true);
+        System.out.println(distance.getDistanceInKm() + " " + distance.isExact());
+
+        int d = (int)distance.getDistanceInKm();
+
+        System.out.println(d);
+    }
+}
