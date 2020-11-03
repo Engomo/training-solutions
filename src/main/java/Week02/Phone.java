92a820f63d4f0d3fec11cba5b88df71ef6eb0096
Engomo <laszlo.vadkert@gmail.com>
2020-11-03 22:47:31 +0100

    phone.java javítva

--- src/main/java/Week02/Phone.java
+++ src/main/java/Week02/Phone.java
@@ -1,5 +1,7 @@
 package Week02;
 
+import java.util.Scanner;
+
 public class Phone {
     private String type;
     private int mem;
@@ -27,7 +29,26 @@ public class Phone {
     }
 
     public static void main(String[] args) {
-        Phone phone1 = new Phone("Samsung",16);
-        Phone phone2 = new Phone("Apple",8);
+        Scanner scanner = new Scanner(System.in);
+
+        Phone phone1 = new Phone("S",8);
+        Phone phone2 = new Phone("S",8);
+
+        System.out.println("Adja meg az első telefon tipusát: ");
+        phone1.setType(scanner.nextLine());
+        System.out.println("Adja meg az első telefon memória mennyiségét: ");
+        phone1.setMem(scanner.nextInt());
+        scanner.nextLine();
+
+        System.out.println("Adja meg az második telefon tipusát: ");
+        phone2.setType(scanner.nextLine());
+        System.out.println("Adja meg az második telefon memória mennyiségét: ");
+        phone2.setMem(scanner.nextInt());
+        scanner.nextLine();
+
+        System.out.println("Az első telefon tipusa: " + phone1.type + "Memóriája: " + phone1.mem);
+        System.out.println("Az második telefon tipusa: " + phone2.type + "Memóriája: " + phone2.mem);
+
+
     }
 }
