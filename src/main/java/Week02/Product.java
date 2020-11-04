54db08dacc060f8e01013ce569ee3207b2197c86
Engomo <laszlo.vadkert@gmail.com>
2020-11-04 11:23:15 +0100

    week 2 11.04 konzultáció

---
+++ src/main/java/Week02/Product.java
@@ -0,0 +1,33 @@
+package Week02;
+
+public class Product {
+    private String name;
+    private String code;
+
+    public Product(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public boolean areTheyEqual(Product p){
+       return this.name.equals(p.name);
+    }
+
+    public static void main(String[] args) {
+        Product product1 =new Product("Kenyér","111");
+        Product product2 =new Product("Kenyér","112");
+        Product product3 =new Product("Pogácsa","333");
+
+        System.out.println(product1.areTheyEqual(product2));
+        System.out.println(product1.areTheyEqual(product3));
+
+    }
+}
--- src/main/java/array/ArrayHandler.java
+++ src/main/java/array/ArrayHandler.java
@@ -1,17 +1,18 @@
 package array;
 
 public class ArrayHandler {
-    public boolean contains(int[] source, int itemToFind){
-        for (int item : source){
-            if (item == itemToFind){
+    public boolean contains(int[] source, int itemToFind) {
+        for (int item : source) {
+            if (item == itemToFind) {
                 return true;
             }
         }
         return false;
     }
-    public int find(int[] source, int itemToFind){
-        for (int i = 0; i < source.length; i++){
-            if (source[i] == itemToFind){
+
+    public int find(int[] source, int itemToFind) {
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == itemToFind) {
                 return i;
             }
         }
