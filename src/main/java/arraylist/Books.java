52a6314b7f504e06937efd0f4d293979b8e87b73
Engomo <laszlo.vadkert@gmail.com>
2020-11-05 18:54:57 +0100

    Capsules

---
+++ src/main/java/arraylist/Books.java
@@ -0,0 +1,40 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Books {
+
+    private List<String> titles = new ArrayList<>();
+
+    public void add(String title){
+        titles.add(title);
+    }
+
+    public List<String> getTitles() {
+        return titles;
+    }
+
+    public List<String> findAllByPrefix(String prefix){
+        List<String> filteredTitles = new ArrayList();
+        for (String title: titles){
+            if (title.indexOf(prefix) == 0){
+                filteredTitles.add(title);
+            }
+        }
+        return filteredTitles;
+    }
+
+    public static void main(String[] args) {
+
+        Books books = new Books();
+
+        books.add("Java alapok");
+        books.add("Java alapok2");
+        books.add("Phyton alapok2");
+        System.out.println(books.getTitles());
+
+        List<String> titlesStartWhitJava = books.findAllByPrefix("Java");
+        System.out.println(titlesStartWhitJava);
+    }
+}
--- src/main/java/arraylist/Capsules.java
+++ src/main/java/arraylist/Capsules.java
@@ -1,5 +1,46 @@
 package arraylist;
 
-public class Capsules {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Capsules {
+List<String> capsule = new ArrayList<>();
+
+    public List<String> getColor() {
+        return capsule;
+    }
+
+    public void addLast(String color){
+        capsule.add(color);
+    }
+
+    public void addFirst(String color){
+        capsule.add(0, color);
+    }
+
+    public void removeLast(){
+        capsule.remove(capsule.size() - 1);
+    }
+
+    public void removeFirst(){
+        capsule.remove(0);
+    }
+
+    public static void main(String[] args) {
+        Capsules capsules = new Capsules();
+
+        capsules.addLast("fekete");
+        capsules.addLast("fehér");
+        capsules.addLast("piros");
+        System.out.println(capsules.getColor());
+
+        capsules.addFirst("kék");
+        System.out.println(capsules.getColor());
+
+        capsules.removeFirst();
+        System.out.println(capsules.getColor());
+
+        capsules.removeLast();
+        System.out.println(capsules.getColor());
+    }
 }
