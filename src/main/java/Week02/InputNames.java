b845637adb44dbddc24143c273aac15c78722252
Engomo <laszlo.vadkert@gmail.com>
2020-11-05 18:46:47 +0100

    Capsules

--- src/main/java/Week02/InputNames.java
+++ src/main/java/Week02/InputNames.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class InputNames {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-       // String name = new String();
+
         String[] name = new String[5];
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
 
             System.out.println("Adja meg az " + (i + 1) + ". nevet: ");
             name[i] = scanner.nextLine();
