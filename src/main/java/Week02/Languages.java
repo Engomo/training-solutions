0d48729d85059a4c07fef0b63488afae00a06bc3
Engomo <laszlo.vadkert@gmail.com>
2020-11-06 10:25:14 +0100

    konzultáció Languages

---
+++ src/main/java/Week02/Languages.java
@@ -0,0 +1,27 @@
+package Week02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Languages {
+
+    public static void main(String[] args) {
+        List<String> languages = new ArrayList<>();
+        languages.add("Java");
+        languages.add("Python");
+        languages.add("JavaScript");
+
+        for(String i : languages) {
+            if ( i.length() > 5){
+                System.out.println(i);
+        }
+
+        System.out.println(languages);
+
+
+
+        }
+
+
+    }
+}
