cb6dfc30dc601e38223a591da72d4244c9ec12ab
Engomo <laszlo.vadkert@gmail.com>
2020-11-12 22:18:14 +0100

    Napszaktól függő köszönés

---
+++ src/main/java/controlselection/greetings/Greetings.java
@@ -0,0 +1,39 @@
+package controlselection.greetings;
+
+import org.w3c.dom.ls.LSOutput;
+import statements.Time;
+
+import java.time.Clock;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class Greetings {
+
+    private LocalTime time0500 = LocalTime.of(5,00);
+    private LocalTime time0900 = LocalTime.of(9,00);
+    private LocalTime time1830 = LocalTime.of(18,30);
+    private LocalTime time2000 = LocalTime.of(20,00);
+
+
+    public String greetings(LocalTime time){
+        if(time.isAfter(time0500) && time.isBefore(time0900)){
+            return "Jó reggelt!";
+        }
+        if(time.isAfter(time0900) && time.isBefore(time1830)){
+            return "Jó napot!";
+        }
+        if(time.isAfter(time1830) && time.isBefore(time2000)){
+            return "jó estét!";
+        }
+        if(time.isAfter(time2000) || time.isBefore(time0500)){
+            return "Jó éjt!";
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+       Greetings greetings = new Greetings();
+
+        System.out.println(greetings.greetings(LocalTime.of(04,55)));
+    }
+}
