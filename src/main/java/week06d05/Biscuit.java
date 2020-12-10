7b1ad3df9de5306d3b2f6cb9a5d17e317e2f8e7c
Engomo <laszlo.vadkert@gmail.com>
2020-12-10 14:35:05 +0100

    week06d05

---
+++ src/main/java/week06d05/Biscuit.java
@@ -0,0 +1,23 @@
+package week06d05;
+
+public class Biscuit {
+    private BiscuitType type;
+    private int gramAmount;
+
+    public Biscuit(BiscuitType type, int gramAmount) {
+        this.type = type;
+        this.gramAmount = gramAmount;
+    }
+
+    public BiscuitType getType() {
+        return type;
+    }
+
+    public int getGramAmount() {
+        return gramAmount;
+    }
+
+    public static Biscuit of(BiscuitType type, int gramAmount) {
+        return new Biscuit(type, gramAmount);
+    }
+}
