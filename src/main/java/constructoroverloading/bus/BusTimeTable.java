4eaa376df51838a8b1ba03260620386c1c7c1a3f
Engomo <laszlo.vadkert@gmail.com>
2020-12-10 10:49:14 +0100

    konstruktor túlterhelés

---
+++ src/main/java/constructoroverloading/bus/BusTimeTable.java
@@ -0,0 +1,33 @@
+package constructoroverloading.bus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusTimeTable {
+    List<SimpleTime> timeTable = new ArrayList<>();
+
+    public BusTimeTable(List<SimpleTime> timeTable) {
+        this.timeTable = timeTable;
+    }
+
+    public BusTimeTable(int firstHour, int lastHour, int everyMinute) {
+        List<SimpleTime> generatedTimeTable = new ArrayList<>();
+        for(int i = firstHour; i <= lastHour; i++) {
+            generatedTimeTable.add(new SimpleTime(i, everyMinute));
+        }
+        timeTable = generatedTimeTable;
+    }
+
+    public List<SimpleTime> getTimeTable() {
+        return timeTable;
+    }
+
+    public SimpleTime nextBus(SimpleTime actual){
+        for (int i = 0; i < timeTable.size(); i++) {
+            if (timeTable.get(i).difference(actual) >= 0) {
+                return timeTable.get(i);
+            }
+        }
+        throw new IllegalStateException("No more buses today!");
+    }
+}
