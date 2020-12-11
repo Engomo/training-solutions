df745ca1c31f72d645a16ddf360062fa5e487ac6
Engomo <laszlo.vadkert@gmail.com>
2020-12-11 15:42:38 +0100

    week07d05

---
+++ src/main/java/week07d05/Car.java
@@ -0,0 +1,17 @@
+package week07d05;
+
+public class Car extends Vehicle{
+
+    public Car() {
+        super(5, TransmissionType.AUTOMATIC);
+    }
+
+    public Car(int numberOfGears) {
+        super(numberOfGears);
+    }
+
+    public Car(int numberOfGears, TransmissionType transmissionType) {
+        super(numberOfGears, transmissionType);
+    }
+
+}
