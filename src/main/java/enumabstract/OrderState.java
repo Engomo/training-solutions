b268211e2fc1453c58b605fccd43f1a4b4e32d51
Engomo <laszlo.vadkert@gmail.com>
2020-12-22 18:20:58 +0100

    absztrakt metódusok implementálása felsorolásos típusokban

---
+++ src/main/java/enumabstract/OrderState.java
@@ -0,0 +1,47 @@
+package enumabstract;
+
+public enum OrderState implements Deletable {
+
+    NEW {
+        @Override
+        public boolean canDelete() {
+            return true;
+        }
+    },
+
+    CONFIRMED {
+        public boolean canDelete() {
+            return true;
+        }
+    },
+
+    PREPARED {
+        public boolean canDelete() {
+            return true;
+        }
+    },
+
+    ONBOARD {
+        public boolean canDelete() {
+            return false;
+        }
+    },
+
+    DELIVERED {
+        public boolean canDelete() {
+            return false;
+        }
+    },
+
+    DELETED {
+        public boolean canDelete() {
+            return false;
+        }
+    },
+
+    RETURNED {
+        public boolean canDelete() {
+            return false;
+        }
+    }
+}
