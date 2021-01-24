c22a0e14166c9f5c43745ceb8226a2357772aa4d
Engomo <laszlo.vadkert@gmail.com>
2021-01-24 22:27:46 +0100

    week12d01

---
+++ src/main/java/week12d01/GradeRounder.java
@@ -0,0 +1,17 @@
+package week12d01;
+
+public class GradeRounder {
+
+    public int[] roundGrades(int[] grades) {
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[i] < 40) {
+                int floor = grades[i] / 5;
+                int next = (floor + 1) * 5;
+                if (next - grades[i] < 3) {
+                    grades[i] = next;
+                }
+            }
+        }
+        return grades;
+    }
+}
