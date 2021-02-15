f971158df325b716d071444412755a8eebc885cb
Engomo <laszlo.vadkert@gmail.com>
2021-02-15 18:07:48 +0100

    vizsga Histogram

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,4 +1,20 @@
 package exam03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringWriter;
+
 public class Histogram {
+
+    public String createHistogram(BufferedReader reader) throws IOException {
+        String line;
+        StringWriter sw = new StringWriter();
+        while ((line = reader.readLine()) != null) {
+            for (int i = 0; i < Integer.parseInt(line); i++) {
+                sw.write("*");
+            }
+            sw.write("\n");
+        }
+        return sw.toString();
+    }
 }
--- src/test/java/exam03/HistogramTest.java
+++ src/test/java/exam03/HistogramTest.java
@@ -9,15 +9,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HistogramTest {
 
-//    @Test
-//    void createHistogram() throws IOException {
-//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(HistogramTest.class.getResourceAsStream("histogram.txt")))) {
-//            String result = new Histogram().createHistogram(reader);
-////           System.out.println(result);
-//            assertEquals("***\n" +
-//                    "****\n" +
-//                    "**\n" +
-//                    "*\n", result);
-//        }
-//    }
+    @Test
+    void createHistogram() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(HistogramTest.class.getResourceAsStream("histogram.txt")))) {
+            String result = new Histogram().createHistogram(reader);
+//           System.out.println(result);
+            assertEquals("***\n" +
+                    "****\n" +
+                    "**\n" +
+                    "*\n", result);
+        }
+    }
 }
